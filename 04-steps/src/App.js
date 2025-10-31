ce46ae2cffefe2374df801ef5096dbf0959481af
Chu A. <chu.amk22@gmail.com>
2025-10-31 15:29:34 +0100

    exported the Steps component into a stand alone file

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,50 +1,9 @@
-import { useState } from "react";
-
-const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-  "Work hard, Play hard 🍾",
-];
+import Steps from "./components/Steps.js";
 
 function App() {
-  const [step, setStep] = useState(1);
-
-  function handlePrevious() {
-    if (step <= 1) return;
-    setStep(step - 1);
-  }
-
-  function handleNext() {
-    if (step >= messages.length) return;
-    setStep(step + 1);
-  }
-
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-        <div className={step >= 4 ? "active" : ""}>4</div>
-      </div>
-      <div className="message">
-        Step {step}: {messages[step - 1]}
-      </div>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+      <Steps />
     </div>
   );
 }
