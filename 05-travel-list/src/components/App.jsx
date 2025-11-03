46353c5ab78cd4d0d03e2cbfe1d4a91afdb84e8e
Chu A. <chu.amk22@gmail.com>
2025-11-03 15:00:12 +0100

    rendering the list items

--- 05-travel-list/src/components/App.jsx
+++ 05-travel-list/src/components/App.jsx
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="app">
@@ -12,6 +17,7 @@ function App() {
 function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,9 +25,30 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
