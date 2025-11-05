aa5e5e33e1f0dd227c132a7f35a842062c4a9b42
Chu A. <chu.amk22@gmail.com>
2025-11-05 11:45:35 +0100

    thinking about state and lifting up state

--- 05-travel-list/src/components/App.jsx
+++ 05-travel-list/src/components/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
 
 function App() {
+  const [items, setItems] = useState([]);
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form setItems={setItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,9 +22,13 @@ function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ setItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState("1");
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
 
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
@@ -38,12 +43,14 @@ function Form() {
     if (!description) return;
     // alert(`Form submitted ${quantity} ${description}`);
     const newItem = { id: Date.now(), description, quantity, packed: false };
-    initialItems.push(newItem);
     console.log(newItem);
+
+    handleAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
   };
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
@@ -65,11 +72,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
